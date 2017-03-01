c7265df48cb0fe0d83c5a50ab4cf9922e13a916a
Антон Бружмелев < >
2017-03-02 01:37:35 +0300

    Добавлены форматирование, кнопки старт,пауза,стоп

--- components/clock/clock.js
+++ components/clock/clock.js
@@ -9,19 +9,50 @@
         this.mins = this.el.querySelector('.clock__mins');
         this.secs = this.el.querySelector('.clock__secs');
 
+        this.startButton = this.el.querySelector('.clock__start_button');
+        this.startButton.onclick = this.start.bind(this);
+
+        this.pauseButton = this.el.querySelector('.clock__pause_button');
+        this.pauseButton.onclick = this.pause.bind(this);
+
+        this.stopButton = this.el.querySelector('.clock__stop_button');
+        this.stopButton.onclick = this.stop.bind(this);
+
         this.start();
       }
 
       start () {
-        setInterval(() => {
-          let date = new Date();
+        this.timerId = setInterval(() => {
+          let date = new Date(); 
 
-          this.hours.innerHTML = date.getHours();
-          this.mins.innerHTML = date.getMinutes();
-          this.secs.innerHTML = date.getSeconds();
+          this.hours.innerHTML = Clock.format(date.getHours());
+          this.mins.innerHTML = Clock.format(date.getMinutes());
+          this.secs.innerHTML = Clock.format(date.getSeconds());
         }, 1000);
       }
+      
+      pause () {       
+        clearInterval(this.timerId);
+      }
 
+
+      stop () {
+        this.hours.innerHTML = '00';
+        this.mins.innerHTML = '00';
+        this.secs.innerHTML = '00';
+        
+        clearInterval(this.timerId);
+      }
+
+      static format (num){
+        let textNum = num + '';
+
+        let resultStr = textNum;
+        if(textNum.length < 2){
+          resultStr = '0'.repeat((2 - textNum.length)) + textNum;
+        }
+        return resultStr;
+      }
   }
 
   // export
