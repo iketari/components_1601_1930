467a727776e79d88ea08a4f17867573c6a88baab
kovalenkovpu <t085en@gmail.com>
2017-03-02 23:58:39 +0300

    Добавлено оформление css, миллисекунды

--- components/clock/clock.js
+++ components/clock/clock.js
@@ -8,6 +8,7 @@
         this.hours = this.el.querySelector('.clock__hours');
         this.mins = this.el.querySelector('.clock__mins');
         this.secs = this.el.querySelector('.clock__secs');
+        this.msecs = this.el.querySelector('.clock__msecs');
 
         this.stopEl = this.el.querySelector('.clock__stop');
         this.startEl = this.el.querySelector('.clock__start');
@@ -33,7 +34,7 @@
        * @param  {MouseEvent} event
        */
       _onClickLog (event) {
-        console.log(`Нажата клвиша: ${event.target.textContent}`);
+        console.log(`Нажата клавиша: ${event.target.textContent}`);
       }
 
       /**
@@ -52,26 +53,42 @@
        * @param  {MouseEvent} event
        */
       _onStopClick (event) {
-          event.preventDefault();
-
-          this.stop();  
-          this._onClickLog(event);
+        event.preventDefault();
+        
+        this.stop();  
+        this._onClickLog(event);
+      }
+      
+      /**
+       * Красивый вывод времени
+       * @param  {date}
+       * @returns {date}
+       */
+      _checkTime(time) {
+        if (time < 10) return ("0" + time);
+        return time;
+      }
+    
+      _checkTimeMsecs(time) {
+        if (time < 10) return ("00" + time);
+        if (time < 100) return ("0" + time);
+        return time;
       }
 
       start () {
         this._tid = setInterval(() => {
           let date = new Date();
 
-          this.hours.innerHTML = date.getHours();
-          this.mins.innerHTML = date.getMinutes();
-          this.secs.innerHTML = date.getSeconds();
-        }, 1000);
+          this.hours.innerHTML = this._checkTime.call(this, date.getHours());
+          this.mins.innerHTML = this._checkTime.call(this, date.getMinutes());
+          this.secs.innerHTML = this._checkTime.call(this, date.getSeconds());
+          this.msecs.innerHTML = this._checkTimeMsecs.call(this, date.getMilliseconds());
+        }, 100);
       }
 
       stop () {
         clearInterval(this._tid);
       }
-
   }
 
   // export
