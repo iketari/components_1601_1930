a3ae0a1d34e1a52225b2e66732d49e908e524e89
Dmitry Markov <forall@stylive.com>
2017-03-04 11:14:03 +0200

    project barebone

---
+++ DmitryMarkov/fancyClock/app.js
@@ -0,0 +1,12 @@
+(function () {
+  'use strict'
+
+  // import
+  let Clock = window.Clock
+
+  let el = document.querySelector('.clock')
+
+  //console.log(el.querySelector('.clock__hours'))
+
+  let clock = new Clock({el})
+})()
---
+++ DmitryMarkov/fancyClock/components/clock/clock.js
@@ -0,0 +1,99 @@
+(function () {
+  'use strict';
+
+  class Clock {
+      constructor (opts) {
+        this.el = opts.el
+
+        this.hours = this.el.querySelector('.clock__hours')
+        this.mins = this.el.querySelector('.clock__mins')
+        this.secs = this.el.querySelector('.clock__secs')
+
+        this.stopEl = this.el.querySelector('.clock__reset')
+        this.startEl = this.el.querySelector('.clock__start')
+        this.pauseEl = this.el.querySelector('.clock__pause')
+
+        this._onStopClick = this._onStopClick.bind(this)
+        this._onStartClick = this._onStartClick.bind(this)
+        this._onPauseClick = this._onPauseClick.bind(this)
+        this._onClickLog = this._onClickLog.bind(this)
+
+        this._initEvents()
+      }
+
+      /**
+       * Установка обработчиков событий
+       * @private
+       */
+      _initEvents () {
+        this.stopEl.addEventListener('click', this._onStopClick)
+        this.startEl.addEventListener('click', this._onStartClick)
+        this.pauseEl.addEventListener('click', this._onPauseClick)
+        this.start()
+      }
+
+      /**
+       * Логирование кликов!
+       * @param  {MouseEvent} event
+       */
+      _onClickLog (event) {
+        console.log(`Нажата клвиша: ${event.target.textContent}`);
+      }
+
+      /**
+       * Дейстиве при старте часиков
+       * @param  {MouseEvent} event
+       */
+      _onStartClick (event) {
+        event.preventDefault()
+
+        this.start()
+        this._onClickLog(event)
+      }
+
+      /**
+       * Дейстиве при стопе часиков
+       * @param  {MouseEvent} event
+       */
+      _onStopClick (event) {
+          event.preventDefault()
+
+          this.stop()
+          this._onClickLog(event)
+      }
+
+      /**
+       * Дейстиве при паузе часиков
+       * @param  {MouseEvent} event
+       */
+      _onPauseClick (event) {
+          event.preventDefault()
+
+          this.pause()
+          this._onClickLog(event)
+      }
+
+      start () {
+        this._tid = setInterval(() => {
+          let date = new Date()
+
+          this.hours.innerHTML = date.getHours()
+          this.mins.innerHTML = date.getMinutes()
+          this.secs.innerHTML = date.getSeconds()
+        }, 1000)
+      }
+
+      stop () {
+        clearInterval(this._tid)
+      }
+
+      pause () {
+        clearInterval(this._tid)
+      }
+
+  }
+
+  // export
+  window.Clock = Clock
+
+})();
